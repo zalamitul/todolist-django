5e6882cfa8d12fad776909a182711c104197f70f
Mitul Zala <68507627+zalamitul@users.noreply.github.com>
2022-08-31 01:37:07 +0530

    deployment

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -124,3 +124,6 @@ MEDIA_ROOT=BASE_DIR
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
